5832f021c3878042f16d02f0fbcf82f701248761
Chase Joyner <chasezj@stanford.edu>
2024-06-05 13:06:27 -0700

    Added llmjudge

---
+++ llmjudge.py
@@ -0,0 +1,49 @@
+from together import Together
+import sys
+import numpy as np
+
+
+names = {"chase" : "query_generation/chase_together_key.txt"}
+default_name = "chase"
+	
+prompt_prefix = "Respond only yes or no: is the question \"" 
+prompt_middle = "\" relevant for this query \"" 
+prompt_end = "\"?"
+
+with open(names[default_name], 'r+') as f:
+    key = f.readlines()[0]
+    key = key.replace("\n", "")
+    client = Together(api_key=key)
+def calc_percision(bool_array):
+    return np.sum(bool_array) / len(bool_array)
+
+
+def judge(query_top_k, user="chase"):
+	results = []
+	if user != default_name:
+		with open(names[user], 'r+') as f:
+			key = f.readlines()[0]
+			key = key.replace("\n", "")
+			client = Together(api_key=key)
+	for query, top_k in query_top_k:
+		for question in top_k:
+			msgs = [prompt_prefix + question + prompt_middle + query + prompt_end]
+			response = client.chat.completions.create(
+    		model="mistralai/Mistral-7B-Instruct-v0.3",
+    		messages=msgs)
+			answer = response.choices[0].message.content.lower()
+			if "yes" in answer:
+				results.append(1)
+			elif "no" in answer:
+				results.append(0)
+			else:
+				print("Error adding 0")
+				results.append(0)		
+	percision = calc_percision(results)
+	return percision
+		
+def main():
+	pass
+
+if __name__ == "__main__":
+	main()
--- query_generation/together_query_gen.py
+++ query_generation/together_query_gen.py
@@ -3,6 +3,14 @@ from together import Together
 import sys
 import time
 
+rephrase_prefix = "As briefly as possible state the topic of the following question: "
+small_question_data = [
+			"What part of the cell is responsible for ATP creation",
+			"What is the apporximate speed of light",
+			"Which gas is the most abundant in our atmosphere",
+			"Which male organs are responsible for sexual reproduction"
+			]
+
 if len(sys.argv) != 2:
 	print("Error please enter the name of whose together key to use")
 	sys.exit()
@@ -15,11 +23,16 @@ def main():
 	key = key.replace("\n", "")
 	client = Together(api_key=key)
 	start = time.time()
+	msgs = []
+	for question in small_question_data:
+		msgs.append({"role": "user", "content": rephrase_prefix + question})
 	response = client.chat.completions.create(
-    		model="meta-llama/Llama-3-8b-chat-hf",
-    		messages=[{"role": "user", "content": "What are some fun things to do in New York"}])
+    		model="mistralai/Mistral-7B-Instruct-v0.3",
+    		messages=msgs)
 	end = time.time()
-	print(response.choices[0].message.content)
+	print(msgs)
+	for i in range(len(response.choices)):
+		print(response.choices[0].message.content)
 	print("Elapsed time:", end - start)
 	
 
