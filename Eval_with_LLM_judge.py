81d819e47be5679fa9a1f64225c498e72c7e4daf
OrangeRainbow102 <rjunkins@stanford.edu>
2024-06-05 19:29:39 -0700

    Retriever Training Code

---
+++ Eval_with_LLM_judge.py
@@ -0,0 +1,111 @@
+
+
+from sentence_transformers import SentenceTransformer
+from sentence_transformers import SentenceTransformer, CrossEncoder, util
+from rank_bm25 import BM25Okapi
+from sklearn.feature_extraction import _stop_words
+import numpy as np
+import pickle
+import torch
+import string
+from llmjudge import judge
+
+
+MAX_SEQ_LENGTH = 256
+NUM_RESULTS = 10
+TOP_K = 32
+
+
+def read_data(filepath):
+    result = []
+    with open(filepath, 'rb') as file:
+        data = pickle.load(file)
+        result = [entry for entry in data]
+
+    return result
+
+
+def tokenizer(text):
+    tokenized_doc = []
+    for token in text.lower().split():
+        token = token.strip(string.punctuation)
+
+        if len(token) > 0 and token not in _stop_words.ENGLISH_STOP_WORDS:
+            tokenized_doc.append(token)
+
+    return tokenized_doc
+
+
+
+def semantic_search(queries, questions, model="msmarco-distilbert-base-v4"):
+    sem_list = []
+
+    biencoder = SentenceTransformer(model)
+    biencoder.max_seq_length = MAX_SEQ_LENGTH
+    cross_encoder = CrossEncoder("cross-encoder/ms-marco-MiniLM-L-6-v2")
+
+    corpus_embeddings = biencoder.encode(questions, convert_to_tensor=True)
+
+    for index, query in queries:
+        question_embedding = biencoder.encode(query, convert_to_tensor=True)
+        hits = util.semantic_search(question_embedding, corpus_embeddings, top_k=TOP_K)
+        hits = hits[0]
+
+        cross_inp = [[query, questions[hit['corpus_id']]] for hit in hits]
+        cross_scores = cross_encoder.predict(cross_inp)
+
+        for idx in range(len(cross_scores)):
+            hits[idx]['cross-score'] = cross_scores[idx]
+
+        sem_list.append((index, [questions[hit['corpus_id']] for hit in hits[:NUM_RESULTS]]))
+
+    return sem_list
+
+def semantic_search_No_Cross_Encoder(queries, questions, model="msmarco-distilbert-base-v4"):
+    sem_list = []
+
+    biencoder = SentenceTransformer(model)
+    biencoder.max_seq_length = MAX_SEQ_LENGTH
+
+    corpus_embeddings = biencoder.encode(questions, convert_to_tensor=True)
+
+    for index, query in queries:
+        question_embedding = biencoder.encode(query, convert_to_tensor=True)
+        hits = util.semantic_search(question_embedding, corpus_embeddings, top_k=TOP_K)
+        hits = hits[0]
+        sem_list.append((index, [questions[hit['corpus_id']] for hit in hits[:NUM_RESULTS]]))
+    return sem_list
+
+
+def main():
+    fine_tuned_model = SentenceTransformer('fine_tuned_small_synthetic')
+
+    with open('test_data.pkl', 'rb') as file:
+        test_data = pickle.load(file)
+
+    if torch.cuda.is_available():
+        print("Warning: No GPU found")
+
+    queries_filepath = "test_queries_100.pkl"
+    questions_filepath = "train_data.pkl"
+
+    questions = read_data(questions_filepath)
+
+    queries = read_data(queries_filepath)
+
+    print(queries[:3])
+
+    new_query_index = [(query[0], query[2]) for query in queries]
+
+    sem_list = semantic_search(new_query_index, questions)
+    result = [(queries[sem_list[i][0]][2], sem_list[i][1]) for i in range(2)] #range(len(sem_list))
+
+    print(result)
+
+    percision = judge(result)
+    print(percision)
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ train_retriever.py
@@ -0,0 +1,127 @@
+
+import pickle
+import torch
+from sentence_transformers import InputExample, datasets
+from sentence_transformers.evaluation import InformationRetrievalEvaluator
+import wandb
+from sentence_transformers import SentenceTransformer, losses
+def load_synthetic_small_data(orig_data, rewritten_data):
+    with open(rewritten_data, 'rb')as file:
+        rewrittens = pickle.load(file)
+
+    with open(orig_data, 'rb') as file:
+        orig = pickle.load(file)
+
+
+    rewrittens.sort(key = lambda x: x[0])
+
+    data = [] #Formatted (Synthetic Query, Document)
+    for i in range(len(rewrittens)):
+        data.append((rewrittens[i][2], orig[rewrittens[i][0]]))
+    return data
+
+def get_eval_dictionaries(eval_data):
+    #data is of type [(query1, doc1), (query2, doc2), ...]
+    #returns a dictionary mapping query keys to queries
+    queries = {}
+    documents = {}
+    for i in range(len(eval_data)):
+        queries[str(i)] = eval_data[i][0]
+        documents[str(i)] = eval_data[i][1]
+
+    relevent_docs = {}
+    for i in range(len(eval_data)):
+        relevent_docs[str(i)] = set(str(i))
+
+    return queries, documents, relevent_docs
+
+def load_binary_dataset_retriever(train_data, test_data, val_data, batch_size=8):
+
+    train_examples = []
+    # Creating the Input Examples with format (anchor, positive, negative)
+    for entry in train_data:
+        train_examples.append(InputExample(texts=[entry[0], entry[1]]))
+
+    test_examples = []
+    for entry in test_data:
+        test_examples.append(InputExample(texts=[entry[0], entry[1]]))
+
+    val_examples = []
+    for entry in val_data:
+        val_examples.append(InputExample(texts=[entry[0], entry[1]]))
+
+    train_dataloader = datasets.NoDuplicatesDataLoader(train_examples, batch_size=batch_size)
+
+    return train_dataloader, test_examples, val_examples
+
+
+def callback_model(score, epoch, steps):
+    wandb.log({"train/epoch": epoch,
+               "train/steps": steps,
+               "val/score": score})
+def main():
+
+    #See this : https://sbert.net/docs/package_reference/sentence_transformer/losses.html#multiplenegativesrankingloss
+
+
+    train_data = load_synthetic_small_data("train_data.pkl", "train_queries_100.pkl")
+    test_data = load_synthetic_small_data("test_data.pkl", "test_queries_100.pkl")
+    val_data = load_synthetic_small_data("valid_data.pkl", "valid_queries_100.pkl")
+
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    train_dloader, test_examples, val_examples = load_binary_dataset_retriever(train_data, test_data, val_data)
+
+    val_queries, val_corpus, val_relevent_docs = get_eval_dictionaries(val_data)
+    val_evaluator = InformationRetrievalEvaluator(queries=val_queries, corpus=val_corpus, relevant_docs=val_relevent_docs, map_at_k=[10])
+    #val_evaluator.primary_metric = 'cosine_accuracy@10'
+
+
+    test_queries, test_corpus, test_relevent_docs = get_eval_dictionaries(test_data)
+    test_evaluator = InformationRetrievalEvaluator(queries=test_queries, corpus=test_corpus, relevant_docs=test_relevent_docs, map_at_k=[10])
+    #test_evaluator.primary_metric = 'cosine_accuracy@10'
+
+
+    # model.evaluate(evaluator)
+    # >> > 0.8001651527663088
+
+
+    model = SentenceTransformer("msmarco-distilbert-base-v4").to(device)
+    loss = losses.MultipleNegativesRankingLoss(model)
+
+
+    num_epochs = 3
+    warmup_steps = int(len(train_dloader) * num_epochs * 0.1)
+
+
+    result_pre_fine_tuning = model.evaluate(test_evaluator)
+    print("Pre-Fine Tuning Evaluation on Test Set: ", result_pre_fine_tuning)
+
+    valresult_pre_fine_tunign = model.evaluate(val_evaluator)
+    print("Pre-Fine Tuning Eval on Eval Set : ", valresult_pre_fine_tunign)
+    print("Primary metric is : ", val_evaluator.primary_metric)
+
+    model.save('pre-trained_small_synthetic')
+
+    model.fit(train_objectives=[(train_dloader, loss)],
+              epochs=num_epochs,
+              warmup_steps=warmup_steps,
+              show_progress_bar=True,
+              evaluator=val_evaluator,
+              evaluation_steps=10,
+              callback=callback_model)
+
+              # checkpoint_save_steps=10,
+              # checkpoint_path='checkpoints')
+
+
+    result_post_fine_tuning = model.evaluate(test_evaluator)
+    print("Post-Fine Tuning Evaluation on Test Set : ", result_post_fine_tuning)
+
+    model.save('fine_tuned_small_synthetic')
+
+
+if __name__ == '__main__':
+    main()
+
+
